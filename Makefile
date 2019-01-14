cb3a4842168abbb620892e39706a35b790cb2bc2
David Williams <david@aveday.net>
2019-01-14 11:29:20 +1030

    Initial commit. Soft UART between ATmega & ATtiny

---
+++ Makefile
@@ -0,0 +1,39 @@
+BAUD=19200
+# MCU=attiny85
+FREQ=8000000 # 16 Mhz
+PROGRAMMER=usbasp
+
+CC=avr-gcc
+CFLAGS=-DF_CPU=${FREQ} -mmcu=${MCU} -Wall -Werror -Wfatal-errors -Wextra -Os
+
+SRC=${wildcard src/*.c}
+OBJ=${SRC:src/%.c=obj/%.o}
+DEP=${OBJ:.o=.d}
+
+.PHONY: all flash clean
+
+all: main.hex
+	@echo ${MCU}
+	@avr-size --mcu=${MCU} -C main.elf
+
+main.elf: ${OBJ}
+	@echo LD $@
+	@${CC} ${CFLAGS} -o $@ $^
+
+main.hex: main.elf
+	@avr-objcopy -j .text -j .data -O ihex $^ $@
+
+-include ${DEP}
+
+obj/%.o: src/%.c
+	@echo CC $@
+	@mkdir -p obj
+	@${CC} ${CFLAGS} -Isrc -MMD -MP -c $< -o $@
+
+flash: main.hex
+	echo ${PROGRAMMER} ${MCU}
+	@avrdude -p${MCU} -c${PROGRAMMER} -b${BAUD} -V -U flash:w:$<
+	@avr-size --mcu=${MCU} -C main.elf
+
+clean:
+	@rm -rf obj *.elf *.hex
---
+++ src/main.c
@@ -0,0 +1,43 @@
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+#include "softuart.h"
+
+#ifdef __AVR_ATmega328P__
+#endif
+#ifdef __AVR_ATtiny85__
+#endif
+
+#define SIG0 'N'
+#define SIG1 'F'
+#define LED_ON() ( PORTB |= _BV(PB4) )
+#define LED_OFF() ( PORTB &= ~(_BV(PB4)) )
+
+int main ()
+{
+  softuart_init();
+  DDRB |= _BV(PB4); // LED pin
+  softuart_turn_rx_on();
+  sei();
+
+	while(1) {
+#ifdef __AVR_ATmega328P__
+    while (softuart_transmit_busy());
+    softuart_putchar(SIG0);
+    LED_ON();
+
+    _delay_ms(1000);
+
+    while (softuart_transmit_busy());
+    LED_OFF();
+    softuart_putchar(SIG1);
+
+    _delay_ms(1000);
+#endif
+#ifdef __AVR_ATtiny85__
+    char b = softuart_getchar();
+    if (b == SIG0) LED_ON();
+    if (b == SIG1) LED_OFF();
+#endif
+	}
+}
